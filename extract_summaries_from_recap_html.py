783bbdee0351768bb6251e4fbe0caebf6977ab63
Soichiro Murakami <plan.sk8.b@gmail.com>
2020-05-29 03:03:27 +0900

    add tqdm and progress

--- extract_summaries_from_recap_html.py
+++ extract_summaries_from_recap_html.py
@@ -6,12 +6,46 @@ import re, datetime
 import argparse
 from bs4 import BeautifulSoup
 import urllib2
+from tqdm import tqdm
+import glob
+import random 
+
+# def setProxy():
+#     proxy_list = [
+#         "50.206.25.106:80",
+#         "50.206.25.111:80",
+#         "50.206.25.107:80",
+#         "50.206.25.108:80",
+#         "50.206.25.109:80",
+#         "50.206.25.110:80",
+#         "50.206.25.104:80",
+#         "68.188.59.198:80",
+#         "71.13.131.142:80",
+#         "52.179.231.206:80",
+#         "54.156.164.61:80",
+#         "12.139.101.100:80",
+#         "108.74.113.180:80",
+#         "68.185.57.66:80",
+#         "12.139.101.98:80",
+#         "103.105.49.53:80"]
+#     ip = random.choice(proxy_list)
+#     proxy = {'http':'http://{0}'.format(ip)}
+#     print(proxy)
+#     proxy_handler = urllib2.ProxyHandler(proxy)
+#     opener = urllib2.build_opener(proxy_handler)
+#     urllib2.install_opener(opener)
 
 def extract_summary(input_filenames, output_folder):
-    for index, filename in enumerate(input_filenames):
+    downloaded_files = glob.glob(os.path.join(output_folder, "*"))
+    for filename in tqdm(input_filenames):
+        # output file
+        out_file = os.path.join(output_folder, filename)
+        if out_file in downloaded_files:
+            continue
+        
         game_id = filename[:filename.index("_")]
         recap_url = "http://www.espn.com/mlb/recap?"+game_id
-
+            
         try:
             response = urllib2.urlopen(recap_url)
         except urllib2.HTTPError, e:
@@ -28,14 +62,12 @@ def extract_summary(input_filenames, output_folder):
         if article == None:
             return
         paras = article.find_all('p')
-        out_file = output_folder + filename
+        # save a file
         with codecs.open(out_file, "w+", "utf-8") as f:
             for para in paras:
                 f.write(para.get_text().replace("\n"," "))
                 f.write("\n")
         f.close()
-        if index%1000 == 0:
-            print index
         time.sleep(1)  # delay between url request
 
 
